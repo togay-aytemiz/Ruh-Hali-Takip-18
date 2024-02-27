56989caa405f225a09da4bb07eb532845fa7e6e5
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-27 18:06:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import diaryData from "./diaryData";
+import DiaryCard from "./components/DiaryCard";
+import Menu from "./components/Menu";
+import "./styles.css";
+export default function App() {
+  /* Challenge 
+
+İki DiaryCard elementi şu anda aşağıdaki verilerle sabit olarak kodlanmıştır. Göreviniz, diaryData.js dosyasındaki 14 günlük verinin tamamı için otomatik olarak DiaryCard elementleri oluşturmak üzere döngünün gücünü kullanmaktır. 
+
+    1. Sabit kodlanmış DiaryCard elementlerinin her ikisi de silinmeli ve yerine toplam 14 
+       otomatik olarak oluşturulan elementler eklenmeli. 
+    
+    2. İlk iki kart orijinal kodlanmış kartlarla aynı görünmeli ve geri kalanı 
+       kartlar benzer görünmelidir. 
+        
+    3. Kodu kısa ve anlaşılır hale getiriniz. 
+*/
+
+  return (
+    <div>
+      <Menu skip={false} />
+      <main className="main-container">
+        <div className="sub-header">
+          <h3>Nisan Raporu</h3>
+        </div>
+
+        {/* ------------ Challenge Section ----------- */}
+        {diaryData.map((diaryItem) => {
+          const { id } = diaryItem;
+          return <DiaryCard key={id} {...diaryItem} />;
+        })}
+      </main>
+    </div>
+  );
+}
